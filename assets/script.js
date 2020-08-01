f6192dc66ec772eaa99341bd61123b7473bbc6e3
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-08-01 16:48:09 -0400

    updates and animation

--- assets/script.js
+++ assets/script.js
@@ -27,18 +27,15 @@ $(document).ready(function () {
 
     const movieHeader = document.querySelector("#movie-header");
     const movieGrid = document.querySelector("#movie-grid");
-    const result1 = document.querySelector("#result1");
-    const result2 = document.querySelector("#result2");
-    const result3 = document.querySelector("#result3");
-    const result4 = document.querySelector("#result4");
 
     // Shuffles the deck of cards
     shuffleBtn.onclick = function () {
         shuffleBtn.style.display = "none";
         pickCardBtn.style.display = "block";
+        // Showing 3 ways to achieve same result
         $("#runtimeBlock").show();
         $("#ratingBlock").css("display", "block");
-        $("#genreBlock").css("display", "block");
+        $("#genreBlock").attr("class", "fieldset center-icons");
     }
 
     // Validate Rating & Genre Selection
